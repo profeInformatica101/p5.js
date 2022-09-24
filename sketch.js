ff28e6d97739b958a9116217d623386bb04d86c5
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-09-24 19:56:39 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(480, 120);
+
+}
+
+function draw(){
+    background(204);
+
+    /** Ejemplo Linea: 
+       line(x1, y1, x2, y2);
+    */
+    fill('red'); //nombre del color
+    line(20, 50, 420, 110);
+}
