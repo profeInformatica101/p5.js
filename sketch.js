e91bcfa95dd91fd917ff68e496e6e044ab4c4500
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-30 16:56:43 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,49 +1,50 @@
 
+var x_ellipse = 0;
+var y_ellipse = 0;
 //corre solo una vez cuando inicia el programa
 function setup(){
-    console.log("setup");
+
+    console.log("setup ");
     
     //createCanvas: ancho, alto en píxeles
     createCanvas(800,300);
+    //Color de fondo
+    background('#B2B5B5');
 
-    //background RGB : Define el color del fondo
-    // RED --> background(255,0,0);
-    // GREEN --> background(0,255, 0);
-    // BLUE --> background(0,255, 0);
-
+  
+    fill('green');
+    ellipse(x_ellipse,y_ellipse, 50, 50);
 }
 
 //corre continuamente después de la función setup
 function draw(){
+
     console.log("draw");
     
-    background(204);
-    //ellipse(x, y, ancho, alto);
-
-/**                             ( x, y)
- * Dibujo la elipse en posición (50,50) de 80 x 80 
- */
-
-    fill('red'); //nombre del color
-    ellipse(50, 50, 80, 80);
-    
- /**                             
- * Dibujo la elipse de color verde en posición (100,100) de 50x50 
- */   
-    fill(0,255,0); //Color en RGB
-    ellipse(100, 100, 50, 50);
-
-    //Test Circulo con evento 
-    ellipseEnPosicionDelRaton();
+    imprimirTeclaPulsada();
+    moverEllpseConTeclado();
 }
 
-function ellipseEnPosicionDelRaton(){
-    //Evento si se pulsa el botón
-    if(mouseIsPressed){
-        fill('blue');
-    }else{
-        fill('green');
+/**
+ * Función para comprobar las teclas
+ */9
+function imprimirTeclaPulsada(){
+    if(keyIsPressed){
+        console.log(keyCode);
     }
-    ellipse(mouseX, mouseY, 50, 50);
+}
 
+
+function moverEllpseConTeclado(){
+    switch(keyCode){
+        case 38:
+            ellipse(x_ellipse, y_ellipse = y_ellipse - 1, 50, 50);
+            break;
+            case 39:
+            ellipse(x_ellipse = x_ellipse+1, y_ellipse , 50, 50);
+            break;
+        case 40:
+            ellipse(x_ellipse, y_ellipse = y_ellipse + 1, 50, 50);
+            break;
+    }
 }
