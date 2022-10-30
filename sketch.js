4a1c6483fc8566b4616ade56dcad09240261fed5
Pablo <111131311+profeInformatica101@users.noreply.github.com>
2022-10-30 15:32:41 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 //corre solo una vez cuando inicia el programa
 function setup(){
     console.log("setup");
@@ -16,6 +17,33 @@ function setup(){
 function draw(){
     console.log("draw");
     
-      //instrucciones a ejecutar en bucle
+    background(204);
+    //ellipse(x, y, ancho, alto);
+
+/**                             ( x, y)
+ * Dibujo la elipse en posición (50,50) de 80 x 80 
+ */
+
+    fill('red'); //nombre del color
+    ellipse(50, 50, 80, 80);
+    
+ /**                             
+ * Dibujo la elipse de color verde en posición (100,100) de 50x50 
+ */   
+    fill(0,255,0); //Color en RGB
+    ellipse(100, 100, 50, 50);
+
+    //Test Circulo con evento 
+    ellipseEnPosicionDelRaton();
+}
+
+function ellipseEnPosicionDelRaton(){
+    //Evento si se pulsa el botón
+    if(mouseIsPressed){
+        fill('blue');
+    }else{
+        fill('green');
+    }
+    ellipse(mouseX, mouseY, 50, 50);
 
 }
